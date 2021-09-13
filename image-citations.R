0da07f67999d47cfe46bba07688cee8b7a2d66cd
Zoe Turner <zoe.turner2@nottshc.nhs.uk>
2021-09-13 13:34:21 +0100

    Added img to blog and added citation to script. Added links to blogs in the About

--- image-citations.R
+++ image-citations.R
@@ -4,6 +4,8 @@
 
 [Image by Doris Jungo from Pixabay](Image by <https://pixabay.com/users/saramukitza-2447148/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2452106)
 
-Image by <a href="https://pixabay.com/users/susannp4-1777190/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1952024">Susann Mielke</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1952024">Pixabay</a>
-  
-Image by <a href="https://pixabay.com/users/7089643-7089643/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4716287">Please Don't sell My Artwork AS IS</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4716287">Pixabay</a>
+<a href="https://pixabay.com/users/susannp4-1777190/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1952024">Susann Mielke</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1952024">Pixabay</a>
+
+<a href="https://pixabay.com/users/7089643-7089643/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4716287">Please Don't sell My Artwork AS IS</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4716287">Pixabay</a>
+
+<a href="https://pixabay.com/users/lazycat188-18191941/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5548043">Anna Pham</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=5548043">Pixabay</a>
